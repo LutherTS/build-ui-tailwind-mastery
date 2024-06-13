c866445ec155f7496367a38aeade92a50d6fe54a
Luther Tchofo Safo <nintendobserver@gmail.com>
2024-06-13 09:25:16 +0200

    Prettier Tailwind configured.

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,23 @@
 export default function Home() {
   return (
-    <div className="flex text-gray-100 h-screen font-whitney">
-      <div className="bg-gray-900 p-3 space-y-2 overflow-y-scroll">
+    <div className="flex h-screen font-whitney text-gray-100">
+      <div className="space-y-2 overflow-y-scroll bg-gray-900 p-3">
         {Array.from({ length: 40 }, (_, i) => {
           return (
             <div
               key={i + 1}
-              className="bg-white text-gray-800 flex items-center justify-center size-12 rounded-full"
+              className="flex size-12 items-center justify-center rounded-full bg-white text-gray-800"
             >
               {i + 1}
             </div>
           );
         })}
       </div>
-      <div className="flex flex-col bg-gray-800 w-60">
-        <div className="px-3 h-12 shadow-md flex items-center font-ginto text-white">
+      <div className="flex w-60 flex-col bg-gray-800">
+        <div className="flex h-12 items-center px-3 font-ginto text-white shadow-md">
           Tailwind CSS
         </div>
-        <div className="p-3 flex-1 space-y-2 overflow-y-scroll text-gray-300 font-medium">
+        <div className="flex-1 space-y-2 overflow-y-scroll p-3 font-medium text-gray-300">
           <p className="text-white">channel 1 (unread)</p>
           <p className="text-white">channel 2 (unread)</p>
           {Array.from({ length: 38 }, (_, i) => {
@@ -25,11 +25,11 @@ export default function Home() {
           })}
         </div>
       </div>
-      <div className="flex flex-col bg-gray-700 flex-1">
-        <div className="px-3 h-12 shadow-md flex items-center flex-shrink-0">
+      <div className="flex flex-1 flex-col bg-gray-700">
+        <div className="flex h-12 flex-shrink-0 items-center px-3 shadow-md">
           general
         </div>
-        <div className="p-3 flex-1 space-y-4 overflow-y-scroll">
+        <div className="flex-1 space-y-4 overflow-y-scroll p-3">
           {Array.from({ length: 40 }, (_, i) => {
             return (
               <p key={i + 1}>
