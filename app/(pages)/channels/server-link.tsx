b60dedd9c2505e3937f23a0361b1f539ddb5c7d7
Luther Tchofo Safo <nintendobserver@gmail.com>
2024-06-13 14:25:25 +0200

    Turning Link into a block.

--- app/(pages)/channels/server-link.tsx
+++ app/(pages)/channels/server-link.tsx
@@ -13,15 +13,26 @@ export function ServerLink({
   const pathname = usePathname();
   const isPathname = pathname === href;
   return (
-    <Link
-      href={href}
-      className={`${
-        isPathname
-          ? "bg-brand rounded-icon-hover text-white"
-          : "hover:bg-brand rounded-icon hover:rounded-icon-hover bg-gray-700 text-gray-100 hover:text-white"
-      } size-icon flex items-center justify-center transition-all duration-200`}
-    >
-      {children}
-    </Link>
+    <div className="relative">
+      <Link
+        href={href}
+        className={`size-icon peer flex items-center justify-center transition-all duration-200 active:translate-y-1 ${
+          isPathname
+            ? "bg-brand rounded-icon-hover text-white"
+            : "hover:bg-brand rounded-icon hover:rounded-icon-hover bg-gray-700 text-gray-100 hover:text-white"
+        }`}
+      >
+        {children}
+      </Link>
+      <div
+        className={`absolute -left-3 top-0 flex h-full flex-col justify-center ${
+          isPathname
+            ? "*:h-10"
+            : "*:h-5 *:scale-0 *:bg-opacity-0 *:peer-hover:scale-100 *:peer-hover:bg-opacity-100"
+        }`}
+      >
+        <div className="w-1 origin-left rounded-r bg-white transition-all duration-200" />
+      </div>
+    </div>
   );
 }
