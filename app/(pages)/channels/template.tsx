5a55ef60b188b87e5e176f0cb9c28e6225ff4ff9
Luther Tchofo Safo <nintendobserver@gmail.com>
2024-06-17 12:09:29 +0200

    Reverting to a layout.

--- app/(pages)/channels/template.tsx
+++ app/(pages)/channels/template.tsx
@@ -13,6 +13,27 @@ export default async function ChannelsLayout({
     take: 35,
   });
 
+  const unreadServers = await prisma.server.findMany({
+    where: {
+      categories: {
+        some: {
+          channels: {
+            some: {
+              unread: true,
+            },
+          },
+        },
+      },
+    },
+    take: 35,
+  });
+  console.log(unreadServers);
+
+  const unreadServerIdentifiers = unreadServers.map((e, i, a) => {
+    return e.identifier;
+  });
+  console.log(unreadServerIdentifiers);
+
   return (
     <>
       <div className="flex h-screen font-whitney text-gray-100">
@@ -24,16 +45,25 @@ export default async function ChannelsLayout({
             <hr className="mx-2 mt-2 rounded border-t-2 border-t-white/[.06]" />
           </div>
           {servers.map((server) => {
-            return (
-              <ServerLink key={server.id} href={`/channels/${server.id}/98`}>
-                <Image
-                  src={`/feh-characters/character_thumb_${server.id}.png`}
-                  alt={`${server.id}`}
-                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" // trying a default for warning handling
-                  fill
-                />
-              </ServerLink>
-            );
+            // just to see how it works
+            if (unreadServerIdentifiers.includes(server.identifier))
+              return (
+                <ServerLink key={server.id} href={`/channels/${server.id}/98`}>
+                  <Image
+                    src={`/feh-characters/character_thumb_${server.id}.png`}
+                    alt={`${server.id}`}
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" // trying a default for warning handling
+                    fill
+                  />
+                  {/* {server.id} */}
+                </ServerLink>
+              );
+            else
+              return (
+                <ServerLink key={server.id} href={`/channels/${server.id}/98`}>
+                  {server.id}
+                </ServerLink>
+              );
           })}
           {/* // data now instructed from a database // */}
           {/* {Array.from({ length: 40 }, (_, i) => {
@@ -69,4 +99,6 @@ export default async function ChannelsLayout({
 
 /* Notes
 I think the solution I'm going to use finding a way for the /channels route to do a hard refresh. At least for now. Going back from template.tsx to layout.tsx. Or just to experiment I can keep it a template.tsx.
+I see now what a template does, it reloads itself at each click as seen in terms of positioning.
+That means perhaps... My solution is for tha main layout to actually be a template.
 */
